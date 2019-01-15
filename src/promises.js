90b6a29466c4c2cec59d17694127f594ea43de5a
damiensauv <damiensauvalle@gmail.com>
2019-01-15 21:29:45 +0100

    make promise

--- src/promises.js
+++ src/promises.js
@@ -1,9 +1,24 @@
-
-
 const john = "John";
 const arya = "Arya";
 const sensa = "Sensa";
-const say = hero => sentence => console.log(`${hero} says : ${sentence}`);
-const johnSay = say(john); // may be used this way setTimeout(johnSay, 1000, 'hello');
+const say = hero => sentence => Promise.resolve(`${hero} says : ${sentence}`);
+const johnSay = say(john);
 const aryaSay = say(arya);
 const sensaSay = say(sensa);
+
+let check = true;
+
+const interval = setInterval(() => {
+  Promise.all([
+    sensaSay("For the North").then(console.log),
+    aryaSay("The king in the North").then(console.log),
+    new Promise((resolve, reject) => {
+      if (check) {
+        check = false;
+        johnSay("Winter is coming").then(console.log);
+      }
+      resolve();
+    })
+  ]);
+  setTimeout(() => clearInterval(interval), 10000);
+}, 1000);
